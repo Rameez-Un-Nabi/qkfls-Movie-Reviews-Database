c4e2d74fa7480067db02d9fb6fd108d59ad9149b
Rameez-Un-Nabi <124799021+Rameez-Un-Nabi@users.noreply.github.com>
2023-09-10 02:39:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -172,5 +172,5 @@ app.use(express.static(__dirname + '/views'));
 
 let port = process.env.PORT || 8080
 app.listen(port, function () {
-  console.log("To view your app, open this link in your browser: http://localhost:" + port);
+  console.log("To view your app, open this link in your browser adding new text: http://localhost:" + port);
 });
